1a73638610a9ce07bce8a94dd97d455af73cd162
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2023-02-06 14:43:52 +0100

    feat: google sheets connection

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log("index.js loaded")
+
+const sheetId = '1yqTvnhsOXRJ723Dn0vg3p9wkgBP3RrWAVvCAujxXTFI';
+const base = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?`;
+const sheetName = 'yanit';
+const qu = 'Select *'
+const query = encodeURIComponent(qu)
+const url = `${base}&sheet=${sheetName}&tq=${query}`
+
+document.addEventListener('DOMContentLoaded', init)
+
+function init() {
+  fetch(url)
+    .then(res => res.text())
+    .then(rep => {
+      const jsonData = JSON.parse(rep.substring(47).slice(0, -2));
+      console.log(jsonData.table)
+    })
+}
