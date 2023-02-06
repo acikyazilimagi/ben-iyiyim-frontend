855447d5278aaaedfddebf5589a7a97dec9f55a6
fdemir <frndemir@gmail.com>
2023-02-06 16:35:23 +0300

    i

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+import compress from "astro-compress";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [compress()]
+});
