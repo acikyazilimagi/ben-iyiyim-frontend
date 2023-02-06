ab48d87a7cd0c43244798315f35ab607d14d9b06
fdemir <frndemir@gmail.com>
2023-02-06 16:49:36 +0300

    remove cf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,8 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 import compress from "astro-compress";
-
-// https://astro.build/config
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [compress()],
-  output: "server",
-  adapter: cloudflare(),
 });
